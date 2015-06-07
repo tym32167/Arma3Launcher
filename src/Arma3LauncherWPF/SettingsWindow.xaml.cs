e9bd51007d434997e44047b2733b69d2b1cf288b
Artem Muradov <tym32167@gmail.com>
2015-06-07 17:05:34 +0500

    enable BE starts

--- src/Arma3LauncherWPF/SettingsWindow.xaml.cs
+++ src/Arma3LauncherWPF/SettingsWindow.xaml.cs
@@ -42,8 +42,7 @@ namespace Arma3LauncherWPF
 
         private bool CheckFilePath(string path)
         {
-            return !string.IsNullOrEmpty(path) && File.Exists(path) &&
-                   System.String.Compare(System.IO.Path.GetFileName(path).ToLower(), "arma3.exe".ToLower(), System.StringComparison.Ordinal) == 0;
+            return !string.IsNullOrEmpty(path) && File.Exists(path);
         }
 
         public class SettingsWindowViewModel : ViewModelBase
